0b1ac29179bfef156f79cd18e8ad72137da756d3
Daniel <d.gavinho@campus.fct.unl.pt>
2022-09-20 15:50:45 +0100

    Added the statics for the 2 commands to be added -GET_NUMBER -EQUAL_PHONES -PHONE_NOT_EXIST -EQUAL_NUMBERS -NO_EQUAL_NUMBERS

--- src/Main.java
+++ src/Main.java
@@ -13,6 +13,8 @@ public class Main {
     public static final String SET_PHONE      = "SP";
     public static final String SET_EMAIL      = "SE";
     public static final String LIST_CONTACTS  = "LC";
+    public static final String GET_NUMBER     = "GN";
+    public static final String EQUAL_PHONES   = "EP";
     public static final String QUIT           = "Q";
 
     //Constantes que definem as mensagens para o utilizador
@@ -22,6 +24,9 @@ public class Main {
     public static final String CONTACT_REMOVED = "contactBook.Contact removed.";
     public static final String CONTACT_UPDATED = "contactBook.Contact updated.";
     public static final String BOOK_EMPTY = "contactBook.Contact book empty.";
+    public static final String PHONE_NOT_EXIST = "Phone number does not exist.";
+    public static final String EQUAL_NUMBERS = "There are contacts that share phone numbers.";
+    public static final String NO_EQUAL_NUMBERS = "All contacts have different phone numbers.";
     public static final String QUIT_MSG = "Goodbye!";
     public static final String COMMAND_ERROR = "Unknown command.";
 
